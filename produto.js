c1472cbae6871968efa2e8b354185baaf3ea5d2c
frankliniruiz-tech <franklin.iruiz@escola.pr.gov.br>
2026-03-25 11:53:37 +0000

    versao1

--- produto.js
+++ produto.js
@@ -1,1 +1,8 @@
+const produto = "Mochila Impermiável";
+let preco = 189.90;
+let estoque = 14;
+const categoria = "acessórios";
 
+console.log (`A ${produto}  custa R$${preco.toFixed(2)}.`);
+console.log (`Temos ${estoque} intens no estoque.`);
+console.log (`O produto pertence a categoria ${categoria}.`);
