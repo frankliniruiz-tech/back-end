da515f97bc415b71134bae58c6b5fc4de28e920d
frankliniruiz-tech <franklin.iruiz@escola.pr.gov.br>
2026-05-18 13:52:06 +0000

    versao2

---
+++ desafio.js
@@ -0,0 +1,4 @@
+// Declarar 5 variáveis para representar um produt.
+// Crie duas variáveis numéricas e exiba uma soma .
+// Declare preço e quantidade. Calcular o valor total, o valor com 20% quantos produtos inteiros com R$500,00 e quanto sobra de troco. 
+
